643704e438f3ab2af1b6351d5886085b274e0d94
James Dorfman <dorfmanjames@gmail.com>
2017-08-25 20:56:04 -0400

    Updated project folder names to Scrive

---
+++ ImageFix.swift
@@ -0,0 +1,26 @@
+//
+//  ImageFix.swift
+//  GoogleVision
+//
+//  Created by James Dorfman on 2017-08-07.
+//  Copyright © 2017 James Dorfman. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIImage {
+    func fixOrientation() -> UIImage {
+        if self.imageOrientation == UIImageOrientation.up {
+            return self
+        }
+        UIGraphicsBeginImageContextWithOptions(self.size, false, self.scale)
+        self.draw(in: CGRect(x: 0, y: 0, width: self.size.width, height: self.size.height))
+        if let normalizedImage: UIImage = UIGraphicsGetImageFromCurrentImageContext() {
+            UIGraphicsEndImageContext()
+            return normalizedImage
+        } else {
+            return self
+        }
+    }
+}
